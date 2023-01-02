a1de9caffbe8124a199d964afc38a073d9fd27a0
johnkirtley <kirtleyj16@gmail.com>
2023-01-01 21:48:08 -0800

    updated logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Husky Tips and Tricks",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "Dedicated to helping fellow Siberian Husky owners",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
