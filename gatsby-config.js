efbb0d58cc28130fa6b0b943a05fd04e08e262af
johnkirtley <kirtleyj16@gmail.com>
2023-01-02 00:07:39 -0800

    updated sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,60 @@
+const siteUrl = 'https://huskytipsandtricks.com'
+
 module.exports = {
+    
   siteMetadata: {
     title: "Husky Tips and Tricks",
     description:
       "Dedicated to helping fellow Siberian Husky owners",
-    siteUrl: 'https://huskytipsandtricks.com'
   },
   plugins: [
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    {
+        resolve: "gatsby-plugin-sitemap",
+        options: {
+            query: `
+            {
+              allSitePage {
+                nodes {
+                  path
+                }
+              }
+              allWpContentNode(filter: {nodeType: {in: ["Post", "Page"]}}) {
+                nodes {
+                  ... on WpPost {
+                    uri
+                    modifiedGmt
+                  }
+                  ... on WpPage {
+                    uri
+                    modifiedGmt
+                  }
+                }
+              }
+            }
+          `,resolveSiteUrl: () => siteUrl,
+          resolvePages: ({
+            allSitePage: { nodes: allPages },
+            allWpContentNode: { nodes: allWpNodes },
+          }) => {
+            const wpNodeMap = allWpNodes.reduce((acc, node) => {
+              const { uri } = node
+              acc[uri] = node
+  
+              return acc
+            }, {})
+            return allPages.map(page => {
+                return { ...page, ...wpNodeMap[page.path] }
+              })
+            },
+            serialize: ({ path, modifiedGmt }) => {
+              return {
+                url: path,
+                lastmod: modifiedGmt,
+              }
+            },
+        },
+    },
     {
       resolve: "gatsby-plugin-sass",
       options: {
