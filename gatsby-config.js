24700a19cb404a67775cf4ac9c2991ec1b653f15
johnkirtley <kirtleyj16@gmail.com>
2023-01-03 12:54:37 -0800

    removed traling plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,6 @@ module.exports = {
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-advanced-sitemap",
-    "gatsby-plugin-remove-trailing-slashes",
     {
         resolve: 'gatsby-plugin-robots-txt',
         options: {
